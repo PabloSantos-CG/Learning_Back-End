56e15d471e7e4ef0a8f6bb319a095af051b28f83
PabloSantos-CG <comercial-pablo@outlook.com>
2024-02-15 16:20:13 -0300

    Practicing

--- nodeJs/ex08/src/index.ts
+++ nodeJs/ex08/src/index.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import router from "./routes/test";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
-app.listen(process.env.PORT || 3000, () => console.log("\nPorta ativa!"));
+app.listen(process.env.PORT, () => console.log("\nPorta ativa!"));
